5d387a299ffcc4471d0610315268402877b43c52
poyonkie <poyonkie@gmail.com>
2017-10-11 21:10:53 -0500

    dev/Basic template 'create-react-app' based'

--- src/components/Global/Content.jsx
+++ src/components/Global/Content.jsx
@@ -12,22 +12,7 @@ class Content extends Component {
 	constructor() {
 		super();
 
-		this.state = {
-			thevalue: '',
-			thevaluebis: ''
-		}
-		this.onChange = this.onChange.bind(this);
-	}
-
-	onChange(e) {
-		switch(e.target.id) {
-			case 'input-a':
-				this.setState({ thevalue: e.target.value });
-				break;
-			case 'input-b':
-				this.setState({ thevaluebis: e.target.value });
-				break;
-		}
+		this.state = {};
 	}
 
   render() {
@@ -35,8 +20,7 @@ class Content extends Component {
     return (
       <div className="Content">
       	<p className="Content-intro">
-      	  <input id="input-a" value={this.state.thevalue} onChange={this.onChange} />
-      	  <input id="input-b" value={this.state.thevaluebis} onChange={this.onChange} />
+      		Content!!!
       	</p>
       	<h2>{this.thevalue}</h2>
       </div>
